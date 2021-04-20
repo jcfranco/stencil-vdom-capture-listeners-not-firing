568a6726f5649f9166ba96c0525f43ececa7ba80
JC Franco <jfranco@esri.com>
2021-04-19 18:41:59 -0700

    set up repro case

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,22 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, Listen } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
+  @Listen('keydown', { capture: true })
+  handleKeyDownCapture() {
+    console.log('@Listen: keydown + capture');
+  }
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+  @Listen('keydown')
+  handleKeyDown() {
+    console.log('@Listen: keydown');
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <input onKeyDown={() => console.log('JSX: onKeyDown')}
+                  onKeyDownCapture={() => console.log('JSX: onKeyDownCapture')} />;
   }
-}
+};
